58f716d114f61b46717dfa935f0d48489fe8b378
MinKhoy <khoiminhvo32@gmail.com>
2023-01-19 19:16:19 +0700

    Reformat routes

--- webTheory/src/routes/index.js
+++ webTheory/src/routes/index.js
@@ -7,12 +7,12 @@ router.get("/welcome", indexController.getWelcome);
 router.get("/academy", indexController.getAcademy);
 router.get("/academy/sql-injection", indexController.getSQLi);
 router.get("/academy/command-injection", indexController.getCMDi);
-router.get("/academy/file-upload-vulnerabilities", indexController.getFileUpload);
-router.get("/academy/broken-authentication", indexController.getBrokenAuth);
 router.get("/academy/cross-site-scripting", indexController.getXSS);
 router.get("/academy/broken-access-control", indexController.getIDOR);
 router.get("/academy/path-traversal", indexController.getPathTraversal);
-router.get("/academy/information-disclosure", indexController.getInfoDisclosure);
 router.get("/academy/server-side-request-forgery", indexController.getSSRF);
+router.get("/academy/broken-authentication", indexController.getBrokenAuth);
+router.get("/academy/information-disclosure", indexController.getInfoDisclosure);
+router.get("/academy/file-upload-vulnerabilities", indexController.getFileUpload);
 
 module.exports = router;
