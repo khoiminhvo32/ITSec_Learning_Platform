91427649430d1112b69aa08bef3120f694bfde9f
MinKhoy <khoiminhvo32@gmail.com>
2023-01-31 01:15:45 +0700

    Update update_password.php + init 2fa-code.php

--- challenges/Broken Authentication/level2/src/static/html/update_password.php
+++ challenges/Broken Authentication/level2/src/static/html/update_password.php
@@ -14,12 +14,14 @@
   <form class="form-login" method="post">
     <div class="container">
       <label for="uname"><b>Enter your new password:</b></label>
-      <input type="text" placeholder="Enter Password" name="password" required>
+      <input type="text" placeholder="Enter Password" name="new_password" required>
+      <input type="hidden" name="post_username" value="<?php echo $_SESSION['username']; ?>">
 
       <span>
         <?php if (isset($message)) echo $message;?>
       </span>
       <button type="submit" name="button">Submit</button>
+      <p style="left: 200px;">Get back to <a href="/">Login Page</a></p>
     </div>
   </form>
 </body>
--- challenges/Broken Authentication/level2/src/update_password.php
+++ challenges/Broken Authentication/level2/src/update_password.php
@@ -1,25 +1,27 @@
 <?php
 session_start();
+if (!isset($_SESSION['username']))
+  die(header("location: forget_password.php"));
+
 if (isset($_POST['button'])){
     try {
         include "./db.php";
-        $sql = "update users set password = ? where username=?";
+        $sql = "update users set password = MD5(?) where username=?";
         $sth = $database->prepare($sql);
-        $sth->bind_param('ss', $_POST['new_password'] ,$_POST['username']);
+        $sth->bind_param('ss', $_POST['new_password'] ,$_POST['post_username']);
         $sth->execute();
-        $sth->store_result();
-        $sth->bind_result($result);
 
-        if ($sth->num_rows > 0) {
-            $sth->fetch();
-            echo "successful";
+        $result = $sth->affected_rows;
+
+        if ($result != NULL) {
+            $message = $result . " rows inserted.";
         } else {
-            $message = "Username doesn't exist";
+            $message = "Something went wrong!!!";
         }
     } catch (mysqli_sql_exception $e) {
         $message = $e->getMessage();
     }   
 }
 
-include "static/html/update_password.html";
+include "static/html/update_password.php";
 ?>
