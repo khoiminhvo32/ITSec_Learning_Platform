8c1d25013c294a4e060746420f9d3164412ec7e6
MinKhoy <khoiminhvo32@gmail.com>
2023-02-06 20:26:00 +0700

    Update Information Disclosure

---
+++ challenges/Information Disclosure/src/README.md
@@ -0,0 +1,2 @@
+# Another Seems-to-be-Important file :D
+## Here is your flag: VIS{READ_me_and_READ_MoRE}
---
+++ challenges/Information Disclosure/src/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href='https://fonts.googleapis.com/css?family=Barlow' rel='stylesheet'>
+</head>
+<body style="font-family: 'Barlow';">
+
+    <section>
+      <img src="/static/lightbulb-2891372-2409779.png">
+      <link rel="stylesheet" href="/static/css.css">
+      <h1>Super Secure Blog</h1>
+      <p style="font-size:large">
+        This is my "<b>Super Secure</b>" blog. It could not have any security bugs becuz this is only a static website 😁. I dare you to find vulnerabilities here 😒.
+      </p>
+    <span style="font-size:large">
+        I will update more in the future, maybe the solutions?</span><br>
+      <button>@MinKhoy</button>
+    </section>
+    <div class="c1"></div>
+    <div class="c2"></div>
+
+</body>
+</html>
